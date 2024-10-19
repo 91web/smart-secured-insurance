427f452a92897693661a891b354a9cbc075af416
Yusuf Babatunde <yusuf.itexpert@gmail.com>
2024-10-19 01:44:00 -0700

    layout updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,78 @@
-import type { Metadata } from "next";
-import "@fontsource/libre-baskerville";
-import "@fontsource/lato";
-// Supports weights 100-900
-import "@fontsource-variable/jost";
-import "./theme/globals.css";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import ThemeRegistry from "./theme/themeRegistry";
-import dynamic from "next/dynamic";
+import createCache from "@emotion/cache";
+import { useServerInsertedHTML } from "next/navigation";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useMemo, useState } from "react";
 
-export const metadata: Metadata = {
-  title: "Smart Guard",
-  description:
-    "Jersey App Product",
-};
-
-const DynamicWebContextProvider = dynamic(
-  () =>
-    import("@/app/core/contexts/main.context").then(
-      (mod) => mod.MainContextProvider
-    ),
-  {
-    ssr: false,
-  }
-);
-
-export default function RootLayout({
+export default function ThemeRegistry({
+  options,
   children,
-}: Readonly<{
+}: {
+  options: { key: string };
   children: React.ReactNode;
-}>) {
+}) {
+  const { key } = options;
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: "light",
+        },
+        typography: {
+          fontFamily: [`${"Lato"}`].join(","),
+        },
+      }),
+    []
+  );
+
+  const [{ cache, flush }] = useState(() => {
+    const cache = createCache({ key });
+    cache.compat = true;
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+    cache.insert = (...args) => {
+      const serialized = args[1];
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+      return prevInsert(...args);
+    };
+    const flush = () => {
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+    return { cache, flush };
+  });
+
+  useServerInsertedHTML(() => {
+    const names = flush();
+    if (names.length === 0) {
+      return null;
+    }
+    let styles = "";
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
+    return (
+      <style
+        key={cache.key}
+        data-emotion={`${cache.key} ${names.join(" ")}`}
+        dangerouslySetInnerHTML={{
+          __html: styles,
+        }}
+      />
+    );
+  });
+
   return (
-    <html lang="en">
-      <body>
-        <DynamicWebContextProvider>
-          <AppRouterCacheProvider>
-            <ThemeRegistry options={{ key: "mui" }}>{children}</ThemeRegistry>
-          </AppRouterCacheProvider>
-        </DynamicWebContextProvider>
-      </body>
-    </html>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
