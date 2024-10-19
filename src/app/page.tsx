d3955dec82feb321af1b360e772c5c613ae823da
Yusuf Babatunde <yusuf.itexpert@gmail.com>
2024-10-19 02:49:11 -0700

    image Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Box from "@mui/material/Box";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import AppProgressBar from "./components/loading/appProgress";
+import SmartG from "../assets/img/sg-logo.png"
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
@@ -64,6 +66,17 @@ export default function Home() {
         </Box>
         <Box pt={2} px={4} width={"80%"} m={"auto"}>
           <AppProgressBar progress={progress} />
+        </Box>
+
+        <Box
+        height={"fit-content"}
+        margin={"auto"}
+        width={{ xs: "50%", md: "30%" }}
+        textAlign={"center"}
+      >
+        <Box display={"flex"} justifyContent={"center"}>
+          <Image src={SmartG} alt={"SmartGuard logo"} priority={true} />
+        </Box>
         </Box>
       </Box>
     </Box>
